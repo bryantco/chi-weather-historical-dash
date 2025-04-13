aa640e8d4426ed1bc0db17d65d818037e5717096
Bryant Cong <bryant.bcp@gmail.com>
2025-04-13 16:03:21 -0500

    feat: add Python script to request data

---
+++ request_data.py
@@ -0,0 +1,120 @@
+import openmeteo_requests
+import requests_cache
+from retry_requests import retry
+import polars as pl
+from datetime import datetime, timedelta, date
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+start_date = '2021-01-01'
+end_date = (date.today() - timedelta(days=1)).strftime('%Y-%m-%d')
+
+# Request data -------------------------------------------------------------------------------------
+# Request from Open-Meteo API ----
+cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+url = "https://archive-api.open-meteo.com/v1/archive"
+params = {
+    # Lat and lon for O'Hare
+	"latitude": 41.978611,
+	"longitude": -87.904724,
+	"start_date": start_date,
+	"end_date": end_date,
+	"daily": ["temperature_2m_mean", "temperature_2m_max", "temperature_2m_min", "rain_sum", "snowfall_sum", "weather_code"],
+	"timezone": "America/Chicago",
+    'temperature_unit': 'fahrenheit',
+    'wind_speed_unit': 'mph',
+    'precipitation_unit': 'inch'
+}
+
+responses = openmeteo.weather_api(url, params=params)
+
+# Process the response as daily data ----
+response = responses[0]
+
+daily = response.Daily()
+daily_temperature_2m_mean = daily.Variables(0).ValuesAsNumpy()
+daily_temperature_2m_max = daily.Variables(1).ValuesAsNumpy()
+daily_temperature_2m_min = daily.Variables(2).ValuesAsNumpy()
+daily_rain_sum = daily.Variables(3).ValuesAsNumpy()
+daily_snowfall_sum = daily.Variables(4).ValuesAsNumpy()
+daily_weather_code = daily.Variables(5).ValuesAsNumpy()
+
+daily_data = {
+	"date": pl.date_range(
+        	start=datetime.strptime(start_date, '%Y-%m-%d').date(),
+			end=datetime.strptime(end_date, '%Y-%m-%d').date(),
+			interval=timedelta(days=1),
+			closed='both',
+			eager=True
+	)
+}
+
+daily_data["temperature_2m_mean"] = daily_temperature_2m_mean
+daily_data["temperature_2m_max"] = daily_temperature_2m_max
+daily_data["temperature_2m_min"] = daily_temperature_2m_min
+daily_data["rain_sum"] = daily_rain_sum
+daily_data["snowfall_sum"] = daily_snowfall_sum
+daily_data["weather_code"] = daily_weather_code
+
+# Convert to polars df; one row per day, one column per weather metric
+chi_weather_daily_df = pl.DataFrame(daily_data)
+
+# Sync with Postgres -------------------------------------------------------------------------------
+# Check for database existence; create if doesn't exist ----
+load_dotenv()
+
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+
+# Try connecting to the default postgres database
+conn = psycopg2.connect(
+    dbname="postgres",
+    user=DB_USER,
+    password=DB_PASSWORD,
+    host=DB_HOST,
+    port=DB_PORT
+)
+
+conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT) 
+cur = conn.cursor()
+
+# Check if the database exists
+cur.execute("SELECT 1 FROM pg_database WHERE datname = %s", (DB_NAME,))
+exists = cur.fetchone()
+
+# Create the db if it doesn't exist
+if not exists:
+    cur.execute(f'CREATE DATABASE "{DB_NAME}"')
+    print(f"Database '{DB_NAME}' created.")
+else:
+    print(f"Database '{DB_NAME}' already exists.")
+
+# Check for table existence; create if doesn't exist ----
+create_table_query = """
+CREATE TABLE IF NOT EXISTS chi_weather_daily (
+    id SERIAL PRIMARY KEY
+)
+"""
+
+cur.execute(create_table_query)
+conn.commit()
+
+print("Table 'chi_weather_daily' checked/created successfully.")
+
+# Write to the table
+chi_weather_daily_df_pd = chi_weather_daily_df.to_pandas()
+engine = create_engine(os.getenv('DATABASE_URL'))
+chi_weather_daily_df_pd.to_sql('chi_weather_daily', engine, if_exists='append', index=False)
+
+cur.close()
+conn.close()
+
