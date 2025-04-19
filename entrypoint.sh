e62c194663380ca10d0f89efff4991a246729951
Bryant C <bryant.bcp@gmail.com>
2025-04-19 17:50:21 -0500

    feat: move charts and assets back to root

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,21 @@ set -e
 superset db upgrade
 superset init
 
-echo "Importing dashboards"
-superset import-dashboards --username admin --path /app/dashboards
+superset fab create-admin \
+    --username admin \
+    --password admin || true
+
+echo "📦 Importing databases..."
+superset import-datasources --path /app/databases || echo "No databases found."
+
+echo "📦 Importing datasets..."
+superset import-datasources --path /app/datasets || echo "No datasets found."
+
+echo "📦 Importing charts..."
+superset import-charts --path /app/charts || echo "No charts found."
+
+echo "📦 Importing dashboards..."
+superset import-dashboards --username admin --path /app/dashboards || echo "No dashboards found."
 
 # Start the app
 superset run -p 8088 -h 0.0.0.0
