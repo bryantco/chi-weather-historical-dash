b6c28f83b4c4b5d66176fafe07b91b1f0c5eb7e9
Bryant C <bryant.bcp@gmail.com>
2025-04-19 17:38:16 -0500

    feat: add ss username to entrypoint bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ superset db upgrade
 superset init
 
 echo "Importing dashboards"
-superset import-dashboards --path /app/dashboards
+superset import-dashboards --username admin --path /app/dashboards
 
 # Start the app
 superset run -p 8088 -h 0.0.0.0
