5f5dd3ddf9ffdfe8fa66921926d4e6557afa8d07
Bryant C <bryant.bcp@gmail.com>
2025-04-19 22:15:49 -0500

    feat: add embedding and public role

--- superset_config.py
+++ superset_config.py
@@ -1,3 +1,16 @@
 import os
 
+AUTH_TYPE = "AUTH_DB"
 SECRET_KEY = os.environ.get("SUPERSET_SECRET_KEY", "fallback-key")
+PUBLIC_ROLE_LIKE_GAMMA = True
+
+FEATURE_FLAGS = {
+    "ALERT_REPORTS": True,
+    "EMBEDDED_SUPERSET": True,
+    "VERSIONED_EXPORT": False
+}
+
+# Dashboard embedding
+GUEST_ROLE_NAME = "Public"
+GUEST_TOKEN_JWT_SECRET = os.environ.get("GUEST_TOKEN_JWT_SECRET")
+HTTP_HEADERS={"X-Frame-Options":"ALLOWALL"}
