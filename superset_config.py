ee5b7ea4852540e47d6d7b551f1690945504fca8
Bryant C <bryant.bcp@gmail.com>
2025-04-19 22:36:07 -0500

    fix: remove AUTH_DB

--- superset_config.py
+++ superset_config.py
@@ -1,13 +1,12 @@
 import os
 
-AUTH_TYPE = "AUTH_DB"
+# AUTH_TYPE = "AUTH_DB"
 SECRET_KEY = os.environ.get("SUPERSET_SECRET_KEY", "fallback-key")
 PUBLIC_ROLE_LIKE_GAMMA = True
 
 FEATURE_FLAGS = {
     "ALERT_REPORTS": True,
     "EMBEDDED_SUPERSET": True,
-    "VERSIONED_EXPORT": False
 }
 
 # Dashboard embedding
